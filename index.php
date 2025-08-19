4db522a8bc8aa04061fd452437d2f6126974fe51
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2025-08-19 20:53:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'attendance_system');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-
 // Create database connection
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
